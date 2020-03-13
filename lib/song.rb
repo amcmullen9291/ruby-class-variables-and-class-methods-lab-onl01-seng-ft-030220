ac08570d9c72f0af74b9bf16ea06e67757d67d23
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-13 07:42:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,12 +3,14 @@
    attr_accessor :name, :artist, :genre
 
   @@count = 0
+  @@artists = {} 
   
   def initialize(name, artist, genre)
      @name = name
      @artist = artist
      @genre = genre
      @@count +=1 
+     @@artists<< artist
   end   
     #binding.pry 
   
